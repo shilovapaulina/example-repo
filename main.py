2440108efb88f3525a88dfe79304f19fb5635c6d
shilovapaulina <shilovapaulina@gmail.com>
2026-04-16 13:27:24 -0400

    added cleaning code to main python file

--- main.py
+++ main.py
@@ -1,3 +1,9 @@
 import pandas as pd
 df = pd.read_csv('us-news-univ-rankings-2017.csv')
+
+for column in ['Tuition and fees','In-state']:
+    df[column] = df[column].replace('$','')
+    df[column] = df[column].replace(',','')
+    df[column] = df[column].astype(float)
+
 print(df.head())
